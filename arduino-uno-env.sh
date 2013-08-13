f1c7eb56ff23f955c745ba8ffdae827d6e48cc09
Dobrica Pavlinusic <dpavlin@rot13.org>
2013-08-13 14:37:49 +0200

    find Arduino's ttyACM from dmesg output

--- arduino-uno-env.sh
+++ arduino-uno-env.sh
@@ -1,4 +1,5 @@
 echo source this file in current shell
 export board=arduino
-export GOODFET=/dev/ttyACM0
+export GOODFET=`dmesg | grep -C 5 www.arduino.cc | grep ttyACM | cut -d: -f3 | sed 's,^ ,/dev/,'`
+test -z "$GOODFET" && echo "can't find ttyACM for Arduino plug it in and re-run script"
 
